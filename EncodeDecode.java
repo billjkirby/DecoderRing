0694c94746a8160a486831614a10f2d6b33919de
billjkirby <35712013+billjkirby@users.noreply.github.com>
2018-01-22 21:59:24 -0500

    Update EncodeDecode.java

--- EncodeDecode.java
+++ EncodeDecode.java
@@ -2,26 +2,30 @@ import java.io.* ;
 
 public class EncodeDecode {
     public static void main( String args[] ) {
-        
+
         InputStreamReader istream = new InputStreamReader(System.in) ;
         BufferedReader bufRead = new BufferedReader(istream) ;
-        
+
+        String doWhat = null;
+
         try {
             System.out.println("Please Enter 'E' for Encode, 'D' for Decode: ");
-            String doWhat = bufRead.readLine();
+            doWhat = bufRead.readLine();
         }
         catch (IOException err) {
             System.out.println("Error reading line");
         }
-        
-        switch (doWhat) {
-            case 'E':  doWhatString = "Encode";
-                       break;
-            case 'D':  doWhatString = "Decode";
-                       break;
+
+        if (doWhat.equals("E")) {
+            System.out.println("Enter line to Encode!");
         }
-        
-        System.out.println( "You want to " + doWhatString + "!" );
+        else if (doWhat.equals("D")) {
+            System.out.println("Enter line to Decode!");
+        }
+        else {
+            System.out.println("Invalid Entry!");
+        }
+
         System.exit( 0 ); //success
     }
 }

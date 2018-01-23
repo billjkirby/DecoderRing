7786c3ea1899ca50a5ef728fae923a52c2264b20
billjkirby <35712013+billjkirby@users.noreply.github.com>
2018-01-22 21:13:44 -0500

    Update EncodeDecode.java

--- EncodeDecode.java
+++ EncodeDecode.java
@@ -1,6 +1,11 @@
+import java.io.* ;
+
 public class EncodeDecode {
     public static void main( String[] args ) {
-    
+        
+        InputStreamReader istream = new InputStreamReader(System.in) ;
+        BufferedReader bufRead = new BufferedReader(istream) ;
+        
         try {
             System.out.println("Please Enter 'E' for Encode, 'D' for Decode: ");
             String doWhat = bufRead.readLine();
